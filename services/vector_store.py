790c638e0456956bd00d94c476d13c362cb120e3
Trinketer22 <trinketer22@localhost>
2026-02-02 23:38:30 +0300

    feat(VectorStore): documents addition

--- services/vector_store.py
+++ services/vector_store.py
@@ -3,9 +3,9 @@ Vector store service (FAISS wrapper).
 Handles all vector searhc operations
 """
 
-import asyncio
 import logging
 from typing import List, Optional, Callable
+from aiorwlock import RWLock
 from langchain_community.vectorstores import FAISS
 from langchain_core.documents import Document
 
@@ -29,7 +29,7 @@ class VectorStoreService:
         self.headers_storage: Optional[FAISS] = None
         self.storage_path: str = storage_path
         self.headers_path: str = headers_path
-        self.embeder: EmbeddingService = embedding_service.embedder
+        self._lock: RWLock = RWLock()
         self._search_type = search_type
         self._initialized = False
 
@@ -95,7 +95,9 @@ class VectorStoreService:
                 search_fn = lambda: search_storage.similarity_search_by_vector(
                     query_vector, k=top_k
                 )
-        docs = await execute_async(search_fn)
+
+        async with self._lock.reader_lock:
+            docs = await execute_async(search_fn)
 
         # Apply filter
         if filter_fn:
@@ -108,6 +110,42 @@ class VectorStoreService:
 
         return docs
 
+    async def add_documents(self, documents: List[Document]) -> List[str]:
+        if (
+            not self._initialized
+            or self.storage is None
+            or self.headers_storage is None
+        ):
+            raise RuntimeError("VectorStore not initialized")
+        added_ids: List[str] = []
+
+        if len(documents) == 0:
+            return added_ids
+
+        doc_crumbs = [
+            Document(page_content=doc.metadata["crumbs"], id=doc.id)
+            for doc in documents
+            if "crumbs" in doc.metadata
+        ]
+
+        storage = self.storage
+        existing_docs = await self.get_by_ids([doc.id for doc in documents if doc.id])
+        if len(existing_docs) > 0:
+            existing_ids = [doc.id for doc in existing_docs if doc.id]
+            raise RuntimeError(
+                f"Documens with ids {','.join(existing_ids)} already exist"
+            )
+        header_storage = self.headers_storage
+        async with self._lock.writer_lock:
+            added_ids = await execute_async(lambda: storage.add_documents(documents))
+            # Once in a storage lifespan update the flag
+            if not self._storage_updated:
+                self._storage_updated = len(added_ids) > 0
+            if len(doc_crumbs) > 0:
+                await execute_async(lambda: header_storage.add_documents(doc_crumbs))
+
+        return added_ids
+
     # Wraps FAISS get_by_ids
     def get_by_ids(
         self,
@@ -116,11 +154,22 @@ class VectorStoreService:
         if not self._initialized:
             raise RuntimeError("VectorStore not initialized")
 
-        return self.storage.get_by_ids(ids)
+        docs: List[Document] = []
+        async with self._lock.reader_lock:
+            docs = self.storage.get_by_ids(ids)
+        return docs
 
     async def cleanup(self):
         """Cleanup resources."""
-        pass  # FAISS doesn't need cleanup
+        if (
+            not self._initialized
+            or self.storage is None
+            or self.headers_storage is None
+        ):
+            return
+        if self._storage_updated:
+            self.storage.save_local(self.storage_path)
+            self.headers_storage.save_local(self.headers_path)
 
 
 # Global singleton
